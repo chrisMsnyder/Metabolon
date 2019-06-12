0275d04482076e875761908a7ec47753a28ab08d
chrisMsnyder <31325897+chrisMsnyder@users.noreply.github.com>
2019-06-12 16:19:07 -0400

    Add files via upload
    Added block comments

--- Metabolon_RSS_Feed.py
+++ Metabolon_RSS_Feed.py
@@ -1,6 +1,25 @@
 import feedparser
 import datetime
 
+######################################################################################
+#
+# RSS_Feed(company_dict, num_days)
+#
+# Purpose:
+#   This function takes in a dict of companies and an integer of days and iterates over
+#   the RSS feeds within the dict and determines if any of them have been modified within
+#   the number of days passed as a parameter and returns a list of all the companies that
+#   have no activity within the specified number of days.
+#
+# Parameters:
+#   company_dict    - Dictionary object keyed by company names and valued by RSS Feed URLs
+#   num_days        - integer that determines number of days to search through for activity
+#
+# Returns:
+#   List of companies that have no activity within the specified number of days
+#
+#######################################################################################
+
 def RSS_Feed(company_dict, num_days):
     delta = datetime.timedelta(days=num_days)
     now_datetime = datetime.datetime.now() - delta
@@ -13,6 +32,3 @@ def RSS_Feed(company_dict, num_days):
         for f in feed.entries:
             date = f.updated
             date = date[:-6]
-            #print(f.title, '\n', date)
-
-        print('\n\n')
