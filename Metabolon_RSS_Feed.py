71beb08ccf1208c1497ead71268e293f8c48ebd0
chrisMsnyder <31325897+chrisMsnyder@users.noreply.github.com>
2019-06-12 17:17:33 -0400

    Add files via upload
    RSS_Tester updated with list of urls as values within the build dict
    Metabolon_RSS_Feed.py now includes additional comments and some error checking. Also correctly returns list of companies without activity.

--- Metabolon_RSS_Feed.py
+++ Metabolon_RSS_Feed.py
@@ -7,9 +7,9 @@ import datetime
 #
 # Purpose:
 #   This function takes in a dict of companies and an integer of days and iterates over
-#   the RSS feeds within the dict and determines if any of them have been modified within
-#   the number of days passed as a parameter and returns a list of all the companies that
-#   have no activity within the specified number of days.
+#   the entries within each of the RSS feeds within the dict and determines if any of them 
+#   have been modified within the number of days passed as a parameter and returns a list 
+#   of all the companies that have no activity within the specified number of days.
 #
 # Parameters:
 #   company_dict    - Dictionary object keyed by company names and valued by RSS Feed URLs
@@ -21,14 +21,37 @@ import datetime
 #######################################################################################
 
 def RSS_Feed(company_dict, num_days):
+    # check if parameters are correct type
+    if not type(company_dict) is dict:
+        print('Exiting program: Incorrect Parameter 1, please make sure a dictionary is passed')
+        exit()
+    if not type(num_days) is int:
+        print('Exiting program: Incorrect Parameter 2, please make sure an integer is passed')
+        exit()
+
+    #establish a date to check against using the durrent date minue the num_days value
     delta = datetime.timedelta(days=num_days)
     now_datetime = datetime.datetime.now() - delta
-    now_datetime = now_datetime.strftime("%Y-%m-%dT%H:%M:%S")
-    print(now_datetime)
+    check_datetime = now_datetime.strftime("%Y-%m-%dT%H:%M:%S")
+    no_activity_list =[]
 
+    #iterate over each company within the dict
     for company in company_dict:
-        feed = feedparser.parse(company_dict[company])
+        found = False
+        #iterate over each rss feed url within the list value of the dict
+        for url in company_dict[company]:
+            feed = feedparser.parse(url)
 
-        for f in feed.entries:
-            date = f.updated
-            date = date[:-6]
+            #iterate over each entry within the rss feed url
+            for f in feed.entries:
+                date = f.updated
+                date = date[:-6]
+                #check if the modified date of the entry is within the specified time
+                if date > check_datetime:
+                    found = True
+                    break
+            if found == True:
+                break
+        if found == False:
+            no_activity_list.append(company)
+    return(no_activity_list)
--- RSS_Tester.py
+++ RSS_Tester.py
@@ -3,14 +3,19 @@ import datetime
 import Metabolon_RSS_Feed as feed
 
 def main():
-    days = 5
+    num_days = 'hello'
     dict = build_test_dict()
-    feed.RSS_Feed(dict, days)
+    print(feed.RSS_Feed(dict, num_days))
 
 def build_test_dict():
-    dict = {'Reddit': 'http://www.reddit.com/.rss', 
-            'Bill Maher': 'http://billmaher.hbo.libsynpro.com/rss',
-            'Craigslist': 'https://www.craigslist.org/about/best/all/index.rss'
+    dict = {'Reddit': ['http://www.reddit.com/.rss'], 
+            'Bill Maher': ['http://billmaher.hbo.libsynpro.com/rss'],
+            'Craigslist': ['https://www.craigslist.org/about/best/all/index.rss'],
+            'ESPN': ['https://www.espn.com/espn/rss/news', 
+                     'http://www.espn.com/espn/rss/nfl/news', 
+                     'http://www.espn.com/espn/rss/nba/news'
+                     ],
+            'Fox News': ['https://www.foxnews.com/about/rss/.rss']
             }
     return dict
 
